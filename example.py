c9736e39c0319990a603c8d2b9a5bf10f05c258c
noheartpen <3104408599@qq.com>
2024-05-21 22:21:40 +0900

    docs: 增加时间统计

--- example.py
+++ example.py
@@ -1,5 +1,10 @@
 """快速开始的实例 """
 
+import time
+
 from src.pynonjishokei.main import scan_input_string
 
+START_TIME = time.perf_counter()
 print(scan_input_string(""))
+END_TIME = time.perf_counter()
+print(f"耗时:{round((END_TIME - START_TIME) * 1000, 3)}毫秒")
